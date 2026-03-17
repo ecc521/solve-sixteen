1d670dbcfebb844748f7e7bdf25c6e8f6923654e
tuckerwillenborg <tuckerwillenborg@gmail.com>
2026-03-17 14:17:52 +0000

    Secondary: JSDoc for client utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,9 +1,13 @@
 /**
- * Shuffles an array using the Fisher-Yates algorithm.
- * Returns a new array, leaving the original array unmodified.
+ * Shuffles the elements of an array using the Fisher-Yates (aka Knuth) algorithm.
+ * 
+ * This function creates a shallow copy of the input array and shuffles the copy in-place,
+ * ensuring that the original array remains unmodified.
+ * It provides a uniform distribution of permutations.
  *
- * @param {Array} array The array to shuffle.
- * @returns {Array} A new shuffled array.
+ * @template T
+ * @param {T[]} array - The input array to be shuffled. Can contain elements of any type.
+ * @returns {T[]} A new array containing the exact same elements as the input array, but in a randomized order.
  */
 export function shuffleArray(array) {
   const newArray = [...array];
